532ce9f17dd899484f094810e2ed884c14d2d337
dev-mev <40147757+dev-mev@users.noreply.github.com>
2020-02-17 11:57:29 -0800

    configure server for heroku deployment

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import dictionaryApi from "./api/dictionary";
 import wordsApi from "./api/words";
 
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,5 +11,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", dictionaryApi);
 app.use("/api", wordsApi);
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
